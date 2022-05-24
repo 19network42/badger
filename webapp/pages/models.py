54a4898b0080c45f99c3cc9f54b27ecc7f9c6d19
Louise Rondia <louiserondia@gmail.com>
2022-05-24 14:44:28 +0200

    current_event needs to be fixed

--- webapp/pages/models.py
+++ webapp/pages/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from django.utils import timezone
+from datetime import datetime
+import zoneinfo
+import time
+import pytz
 
-# Create your models here.
+utc=pytz.UTC
 
-class Event(models.Model):	
-	date = models.DateTimeField(default=timezone.now)
+class Event(models.Model):
+	date = models.DateTimeField(default=timezone)
 	drinks = models.CharField(max_length = 2)
 	name = models.CharField(max_length = 100)
-	duration = models.IntegerField()
+	end = models.DateTimeField()
+	print(date)
 
+	def	is_current(self):
+		now = datetime.now()
+		now = utc.localize(now)
+		return (self.date < now < self.end)
 
 class Scan(models.Model):
 	uid = models.CharField(max_length=15)
 	date = models.DateTimeField(default=timezone.now)
-
