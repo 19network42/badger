639498f49c082f43870697e5cf3636130e5171c7
Thomas Amighi <tamighi@shi-r11-p4.s19.be>
2022-05-24 12:37:31 +0200

    Clean nfcDisplay.ino

--- embedded/nfcDisplay/nfcDisplay.ino
+++ embedded/nfcDisplay/nfcDisplay.ino
@@ -1,215 +1,184 @@
+//  Librairies : Adafruint_PN532, Adafruit_busIO, WiFiNINA
 
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
-//#include <LiquidCrystal.h>
 #include <WiFiNINA.h>
 
+#include "arduino_secrets.h" 
+
+
+////	Wifi setup
+//	SECRET variables are defined in arduino_secret.h
+
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+WiFiClient client;
+
+////	Server setup
+
+IPAddress server(10,40,6,198); 
+int port = 8000;
+
+int status = WL_IDLE_STATUS;
+
+////	NFC setup
 
 #define PN532_SCK  2
 #define PN532_MOSI 3
 #define PN532_SS   4
 #define PN532_MISO 5
 
-#include "arduino_secrets.h" 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;            // your network key index number (needed only for WEP)
-
-
-int status = WL_IDLE_STATUS;
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-IPAddress server(10,40,6,198);  // numeric IP for Google (no DNS)
-//char server[] = "10.40.6.198";    // name address for Google (using DNS)
-
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-WiFiClient client;
-
-// If using the breakout or shield with I2C, define just the pins connected
-// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
-//#define PN532_IRQ   (2)
-//#define PN532_RESET (3)  // Not connected by default on the NFC Shield
-
 Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
-//const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
-//LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
 
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
+	Serial.begin(9600);
+	while (!Serial) {
+		; // Wait for serial port to connect. Needed for native USB port only
+	}
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-  nfc.begin();
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  while (!versiondata) {
-    Serial.print("Didn't find PN53x board");
-    versiondata = nfc.getFirmwareVersion();
-//    while (1); // halt
-  }
-  
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
-  // Set the max number of retry attempts to read from a card
-  // This prevents us from waiting forever for a card, which is
-  // the default behaviour of the PN532.
-  nfc.setPassiveActivationRetries(0xFF);
-  
-  // configure board to read RFID tags
-  nfc.SAMConfig();
-  
+	String fv = WiFi.firmwareVersion();
+	if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+		Serial.println("Please upgrade the firmware");
+	}
 
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+	//	Identifying nfc reader
 
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
+	nfc.begin();
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
+	uint32_t versiondata = 0;
+	int	tries = 0;
 
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
+	Serial.println("Looking for PN53x board ...");
+	while (!versiondata && tries++ < 5) {
+		versiondata = nfc.getFirmwareVersion();
+	}
 
-    // wait 10 seconds for connection:
-    delay(100);
-  }
-  Serial.println("Connected to WiFi");
-  printWiFiStatus();
+	if (!versiondata) {
+		Serial.println("Failed to find PN53x board.");
+		while (1) {
+			;
+		}
+	}
 
-  
-  while (!Serial) delay(10); // for Leonardo/Micro/Zero
-  Serial.println("Hello!");
-  Serial.println("\nStarting connection to server...");
-  if (client.connect(server, 8000)) {
-      Serial.println("connected to server");
-      // Make a HTTP request: 
-   }
-   else
-   {
-     Serial.println("No connection for you");
-    }
-//  lcd.begin(16,2);
-}}
+	//	Found reader, printing data
+
+	Serial.print("Found chip PN5");
+	Serial.println((versiondata>>24) & 0xFF, HEX); 
+	Serial.print("Firmware ver. ");
+	Serial.print((versiondata>>16) & 0xFF, DEC); 
+	Serial.print('.');
+	Serial.println((versiondata>>8) & 0xFF, DEC);
+
+	// Set the max number of retry attempts to read from a card
+	// This prevents us from waiting forever for a card, which is
+	// the default behaviour of the PN532.
+	nfc.setPassiveActivationRetries(0xFF);
+
+	// configure board to read RFID tags
+	nfc.SAMConfig();
+
+
+	//	Connecting to wifi network
+
+	Serial.print("Attempting to connect to SSID: ");
+	Serial.println(ssid);
+
+	while (status != WL_CONNECTED) {
+		status = WiFi.begin(ssid, pass);
+		delay(100);
+	}
+
+	Serial.println("Connected to WiFi");
+	printWiFiStatus();
+
+	//	Connecting to webapp
+	Serial.println("\nStarting connection to server...");
+	if (client.connect(server, port)) {
+	  Serial.println("connected to server");
+	  // Make a HTTP request: 
+	}
+	else
+		Serial.println("Failed to connect to the server");
+}
 
 void loop() {
-  boolean success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-  
+	boolean success;
+	uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+	uint8_t uidLength;        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
-  Serial.println("Waiting for an ISO14443A card");
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+	//	Waiting for a card to be scanned
+	Serial.println("Waiting for an ISO14443A card");
+	success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
 
-  int len = 0;
-  char id[20];
-  
-  if (success) {
-    Serial.println("Found a card!");
-//    Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-    Serial.print("UID Value: ");
-    for (uint8_t i=0; i < uidLength - 1; i++) 
-    {
-      len += sprintf(id + len, "%X:", uid[i]);
-    }
-    sprintf(id + len, "%X", uid[uidLength - 1]);
-    Serial.println(id);
-  // if you get a connection, report back via serial:
+	int len = 0;
+	char id[20];
 
-    String jsonObject = "{\n\"id\": \"";
-    client.println("POST / HTTP/1.1");
-    client.println("Host: 10.40.6.198");
-    client.println("Content-Type: application/json");
-    client.println("Connection: keep-alive");
-    client.println();
+	if (success) {
+		Serial.println("Found a card!");
+		for (uint8_t i=0; i < uidLength - 1; i++) 
+		{
+			len += sprintf(id + len, "%X:", uid[i]);
+		}
+		sprintf(id + len, "%X", uid[uidLength - 1]);
+		Serial.print("UID Value: ");
+		Serial.println(id);
+		// if you get a connection, report back via serial:
 
-    //BODY
-    client.print(jsonObject);
-    client.print(id);
-    client.println("\"}\n");
-    client.println();
-    // wait 10 seconds for connection:
-    delay(3000);
-  }
-//    Serial.println(id);
-//    lcd.print("Thanks for");
-//    lcd.setCursor(0,1);
-//    lcd.print("scanning!");
-  // Wait 1 second before continuing
-//    delay(3000);
-//    lcd.clear();
-  else
-  {
-    // PN532 probably timed out waiting for a card
-    Serial.println("Timed out waiting for a card");
-  }
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
+		String jsonObject = "{\n\"id\": \"";
+		client.println("POST / HTTP/1.1");
+		client.println("Host: 10.40.6.198");
+		client.println("Content-Type: application/json");
+		client.println("Connection: keep-alive");
+		client.println();
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
+		//BODY
+		client.print(jsonObject);
+		client.print(id);
+		client.println("\"}\n");
+		client.println();
 
-    // do nothing forevermore:
-    while (true);
-}
+		//	wait 3 seconds for connection:
+		delay(3000);
+
+		//	Read response from server
+		while (client.available()) {
+			char c = client.read();
+			Serial.write(c);
+		}
+	}
+	else {
+		// PN532 probably timed out waiting for a card
+		Serial.println("Timed out waiting for a card");
+	}
+
+	// if the server's disconnected, stop the client:
+	if (!client.connected()) {
+		Serial.println();
+		Serial.println("Disconnected from server.");
+		client.stop();
+
+		// do nothing forevermore:
+		while (true)
+			;
+	}
 }
 
 void printWiFiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+	// print the SSID of the network you're attached to:
+	Serial.print("SSID: ");
+	Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+	// print your board's IP address:
+	IPAddress ip = WiFi.localIP();
+	Serial.print("IP Address: ");
+	Serial.println(ip);
 
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+	// print the received signal strength:
+	long rssi = WiFi.RSSI();
+	Serial.print("signal strength (RSSI):");
+	Serial.print(rssi);
+	Serial.println(" dBm");
 }
