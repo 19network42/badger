4fca6e4e9d109bb267375776d5d022c93b35bff8
Aleksandr Buzdin <abuzdin@shi-r11-p3.s19.be>
2022-05-24 15:49:08 +0200

    added students, add_student and update_student pages; added student form

---
+++ webapp/badges/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Student
+
+class StudentForm(ModelForm):
+	class Meta:
+		model = Student
+		fields = ('intra_id', 'login', 'email', 'displayname', 'is_staff')
+
+		#to add some style
+		labels = {
+			'intra_id': '',
+			'login': '',
+			'email': '',
+			'displayname': '',
+			'is_staff': '',
+		}
+		widgets = {
+			'intra_id': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Intra_id'}),
+			'login': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Login'}),
+			'email': forms.EmailInput(attrs={'class':'form-control', 'placeholder': 'Email'}),
+			'displayname': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Name'}),
+			'is_staff': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Staff?'}),
+		}
--- webapp/badges/views.py
+++ webapp/badges/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from .models import Student, Badge, StudentBadge
+from .forms import StudentForm
+from accounts.models import User
+import json
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
-# Create your views here.
+def	students(request, *args, **kwargs):
+	context = {
+		'students': Student.objects.all(),
+	}
+	return render(request, "students.html", context)
+
+def	add_student(request):
+	submitted = False
+	if request.method == "POST":
+		form = StudentForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/add_student?submitted=True')
+	else:
+		form = StudentForm()
+		if 'submitted' in request.GET:
+			submitted = True
+	return render(request, "add_student.html", {'form': form, 'submitted': submitted})
+
+def	update_student(request, student_id):
+	student = Student.objects.get(pk=student_id)
+	form = StudentForm(request.POST or None, instance=student)
+	if form.is_valid():
+		form.save()
+		return redirect('badges:students')
+	return render(request, "update_student.html", {'student': student, 'form': form})
+
