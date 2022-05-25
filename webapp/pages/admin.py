e507b7e9a5a48d96f1d50a7ae1a41c8cf422e7f2
Louise Rondia <louiserondia@gmail.com>
2022-05-25 15:14:32 +0200

    created different modes and amount v1

--- webapp/pages/admin.py
+++ webapp/pages/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Scan, Event
+from .models import Scan, Event, Mode
 
 # Register your models here.
 admin.site.register(Scan)
 admin.site.register(Event)
+admin.site.register(Mode)
