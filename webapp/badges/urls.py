8e473ad0782bbc6b418f44d1fb211bc1ee41739d
Louise Rondia <louiserondia@gmail.com>
2022-05-25 12:11:11 +0200

    merged

--- webapp/badges/urls.py
+++ webapp/badges/urls.py
@@ -1,20 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-<<<<<<< HEAD
-from badges.views import add_student, update_student, students
-=======
-from badges.views import testing_student
->>>>>>> 8ce54b259d19077bd9cf0a06647b73af453d20ff
+from badges.views import add_student, update_student, students, testing_student
 
 app_name = 'badges'
 
 urlpatterns = [
-<<<<<<< HEAD
     path('admin/', admin.site.urls),
     path('students/', students, name = 'students'),
     path('add_student/', add_student, name = 'add_student'),
     path('update_student/<int:student_id>/', update_student, name = 'update_student'),
-=======
     path('test_student/', testing_student, name = 'test_student')
->>>>>>> 8ce54b259d19077bd9cf0a06647b73af453d20ff
 ]
