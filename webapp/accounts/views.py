4a2595dd7f43dfbccd7898cd26f52467e66270da
Aleksandr Buzdin <abuzdin@shi-r12-p1.s19.be>
2022-05-25 15:03:02 +0200

    added login and logout funcitons

--- webapp/accounts/views.py
+++ webapp/accounts/views.py
@@ -8,26 +8,27 @@ from .backends import oauth
 
 
 def login(request):
-    return render(request, "accounts/login.html")
+	return render(request, "accounts/login.html")
 
 
 def authenticate(request):
-    api42 = oauth.create_client('api42')
-    return api42.authorize_redirect(request, settings.API42_REDIRECT_URI)
+	api42 = oauth.create_client('api42')
+	return api42.authorize_redirect(request, settings.API42_REDIRECT_URI)
 
 
 def authorize(request):
-    token = oauth.api42.authorize_access_token(request)
-    user = auth.authenticate(request=request, token=token)
-    if user is None:
-        messages.error(request, 'Sorry, we could not authenticate you.')
-        return redirect('/')
-    else:
-        auth.login(request, user)
-        return redirect('/')
+	token = oauth.api42.authorize_access_token(request)
+	user = auth.authenticate(request=request, token=token)
+	if user is None:
+		messages.error(request, 'Sorry, we could not authenticate you.')
+		return redirect('/')
+	else:
+		auth.login(request, user)
+		return redirect('/')
 
 
 @login_required
 def logout(request):
-    auth.logout(request)
-    return redirect('/')
+	auth.logout(request)
+	messages.success(request, ("You were logged out!"))
+	return redirect('/')
