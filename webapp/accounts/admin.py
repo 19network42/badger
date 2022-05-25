15b3bb7f3d8926985013b8023510b7455cf408e3
Aleksandr Buzdin <abuzdin@shi-r12-p1.s19.be>
2022-05-25 15:56:51 +0200

    small format changes

--- webapp/accounts/admin.py
+++ webapp/accounts/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('login', 'displayname', 'user_id', 'is_staff')
+	list_display = ('login', 'displayname', 'user_id', 'is_staff')
 admin.site.register(User, UserAdmin)
