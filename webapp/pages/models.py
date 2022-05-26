b00e267046a1110adcfe303e0a13794e83e1d750
Aleksandr Buzdin <abuzdin@shi-r12-p1.s19.be>
2022-05-26 15:01:02 +0200

    small upd

--- webapp/pages/models.py
+++ webapp/pages/models.py
@@ -6,25 +6,15 @@ from datetime import datetime
 def two_hours_hence():
 	return timezone.now() + timezone.timedelta(hours=1)
 
-class Type(models.Model):
-	type = models.CharField(max_length = 100)
-	def __str__(self):
-		return str(self.type)
-
-class Amount(models.Model):
-	amount = models.IntegerField()
-	def __str__(self):
-		return str(self.amount)
-
 class Mode(models.Model):
 	amount = models.IntegerField()
-	type = models.ForeignKey(Type, on_delete=models.CASCADE)
+	type = models.CharField(max_length = 100)
 
 class Event(models.Model):
 	date = models.DateTimeField(default=timezone.now)
 	name = models.CharField(max_length = 100)
 	end = models.DateTimeField(default=two_hours_hence)
-	mode = models.ManyToManyField(Mode)
+	modes = models.ManyToManyField(Mode)
 
 	def is_current(self):
 		now = datetime.now()
