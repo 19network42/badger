c34c9e6d0c4422c7b7d334da19acf12d8c55634d
Thomas Amighi <tamighi@shi-r11-p4.s19.be>
2022-06-09 13:40:31 +0200

    Added a search specific scans page

--- webapp/api/urls.py
+++ webapp/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import scan_page, delete_scan, init_page
+from .views import scan_page, delete_scan, init_page, search_scan_page
 
 app_name = 'api'
 
@@ -8,5 +8,6 @@ urlpatterns = [
 	#path('scans', api, name = 'api'),
 	path('scan/', scan_page, name = 'scan'),
 	path('init/', init_page, name = 'init'),
+	path('search_scan/', search_scan_page, name='search_scan'),
 	path('delete_scan/<int:scan_id>/', delete_scan, name = 'delete_scan'),
 ]
