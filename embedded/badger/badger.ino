6a7957631d05a77d0c1adbf49e0ff121ae31ebc1
jvander- <jvander-@student.s19.be>
2022-06-16 11:56:54 +0200

    delete Serial print and prettify the code (using tab instead of space).

--- embedded/badger/badger.ino
+++ embedded/badger/badger.ino
@@ -160,11 +160,7 @@ void	getInitDataFromWebApp(void)
 
 	DeserializationError error = deserializeJson(doc, client);
 
-	if (error)
-	{
-		Serial.print("deserializeJson() failed: ");
-		Serial.println(error.c_str());
-	}
+	if (error){}
 	else
 	{
 		modesFromApp = doc["mode"];
@@ -232,11 +228,9 @@ void	isConnectedToWifi(void)
 	lcd.clear();
 	lcd.print("Wifi connect.");
 	lcd.setCursor(0,1);
-	Serial.println("Wifi connected");
 	lcd.print("OK!");
 	delay(1500);
 	lcd.clear();
-	printWiFiStatus();
 	attachInterrupt(digitalPinToInterrupt(BUTTON), changeMode, FALLING);
 }
 
@@ -254,8 +248,6 @@ void	clientIsConnected(bool reconnection)
 	detachInterrupt(BUTTON);
 	if (reconnection && !client.connected())
 	{
-		Serial.println();
-		Serial.println("Disconnected from server.");
 		lcd.clear();
 		lcd.print("Server connect.");
 		lcd.setCursor(0,1);
@@ -311,82 +303,80 @@ void	getDataFromWebAppUser(void)
 	const char	*msg;
 	JsonArray			led;
 	DynamicJsonDocument doc(capacity);
-  
-  DeserializationError error = deserializeJson(doc, client);
-  
-  if (error)
-  {
-    Serial.print("deserializeJson() failed: ");
-    Serial.println(error.c_str());
-  }
-  led = doc["led"];
-  red = led[0];
-  green = led[1];
-  blue = led[2];
-  turnOnLed(red,green,blue);
-  bool buzzer = doc["buzzer"];
-  if (buzzer)
-    playSuccessBuzzer();
-  else
-    playFailureBuzzer();
-  delay(200);
-  turnOnLed(red,green,blue);
-  delay(300);
-  turnOnLed(0,0,255);
-  msg = doc["msg"];
-  Serial.println(msg);
-  lcd.clear();
-//  lcd.print(msg);
-  scrollingMessage(msg);
-  delay(500);
+
+	DeserializationError error = deserializeJson(doc, client);
+
+	if (error){}
+	else
+	{
+		led = doc["led"];
+		red = led[0];
+		green = led[1];
+		blue = led[2];
+		turnOnLed(red,green,blue);
+		bool buzzer = doc["buzzer"];
+		if (buzzer)
+		{
+			playSuccessBuzzer();
+		}
+		else
+		{
+			playFailureBuzzer();
+		}
+		delay(200);
+		turnOnLed(red,green,blue);
+		delay(300);
+		turnOnLed(0,0,255);
+		msg = doc["msg"];
+		lcd.clear();
+		scrollingMessage(msg);
+		delay(500);
+	}
 }
 
 
 /*
  * Utils method to display a message with scrolling view.
  */
-void  scrollingMessage(const char * msg)
+void	scrollingMessage(const char * msg)
 {
-  uint8_t messageLength;
-  uint8_t lcdLength = 15;
-  uint8_t totalScroll;
+	uint8_t messageLength;
+	uint8_t lcdLength = 15;
+	uint8_t totalScroll;
 
-  lcd.print(msg);
-  delay(300);
-  messageLength = strlen(msg);
-  totalScroll = messageLength - lcdLength;
-  Serial.println(totalScroll);
-  detachInterrupt(BUTTON);
-  for (int i = totalScroll; i >= 0; i--)
-  {
-    lcd.scrollDisplayLeft();
-    delay(250);
-  }
-  attachInterrupt(digitalPinToInterrupt(BUTTON), changeMode, FALLING);
+	lcd.print(msg);
+	delay(300);
+	messageLength = strlen(msg);
+	totalScroll = messageLength - lcdLength;
+	detachInterrupt(BUTTON);
+	for (int i = totalScroll; i >= 0; i--)
+	{
+		lcd.scrollDisplayLeft();
+		delay(250);
+	}
+	attachInterrupt(digitalPinToInterrupt(BUTTON), changeMode, FALLING);
 }
- 
+
 /*
  * Check if the response from the webApp is good to continue
  * process of the program.
  * @return bool : true if the response is correct, False otherwise.
  */
-bool  isResponseFromWebAppOK()
+bool	isResponseFromWebAppOK()
 {
-    char status[64] = {0};
-    client.readBytesUntil('\r', status, sizeof(status));
-    // It should be "HTTP/1.1 201 CREATED"
-    Serial.println(status);
-    if (strcmp(status + 9, "201 Created") != 0) {
-      Serial.print(F("Unexpected response: "));
-      Serial.println(status);
-      return (false);
-    }
-    char endOfHeaders[] = "\r\n\r\n";
-    if (!client.find(endOfHeaders)) {
-      Serial.println(F("Invalid response"));
-      return (false);
-    }
-    return (true);
+	char status[64] = {0};
+	client.readBytesUntil('\r', status, sizeof(status));
+	// It should be "HTTP/1.1 201 CREATED"
+	if (strcmp(status + 9, "201 Created") != 0)
+	{
+		return (false);
+	}
+	char endOfHeaders[] = "\r\n\r\n";
+	if (!client.find(endOfHeaders))
+	{
+		return (false);
+	}
+	return (true);
 }
 
 /*
@@ -394,130 +384,106 @@ bool  isResponseFromWebAppOK()
  * @param String uid : the uid of the card scanned.
  * @param String mode : the actual mode for drinks.
  */
-void  createAndSendHttpRequestUser(String uid, String mode)
+void	createAndSendHttpRequestUser(String uid, String mode)
 {
-  String postData = "{\"id\":\"" + uid + "\",\"mode\":\"" + mode + "\"}";
-  client.print(
-    String("POST ") + SCAN_URL + " HTTP/1.1\r\n" +
-    "Content-Type: application/json\r\n" +
-    "Content-Length: " + postData.length() + "\r\n" +
-    "X-Secret: " + TOKEN_POST + "\r\n" +
-    "\r\n" +
-    postData
-    );
+	String postData = "{\"id\":\"" + uid + "\",\"mode\":\"" + mode + "\"}";
+	client.print(
+		String("POST ") + SCAN_URL + " HTTP/1.1\r\n" +
+		"Content-Type: application/json\r\n" +
+		"Content-Length: " + postData.length() + "\r\n" +
+		"X-Secret: " + TOKEN_POST + "\r\n" +
+		"\r\n" +
+		postData
+	);
 }
 
 /*
  * Try to connect to the server.
  */
-void  connectToWebApp()
+void	connectToWebApp()
 {
-  Serial.println("\nStarting connection to server...");
-  if (client.connect(IPADDRESS_SERVER, PORT))
-  {
-    lcd.clear();
-    Serial.println("Connected to server");
-    lcd.print("Connect. server");
-    lcd.setCursor(0,1);
-    lcd.print("OK!");
-    turnOnLed(0,0,255);
-  }
-  else
-  {
-    lcd.clear();
-    Serial.println("Failed to connect to the server");
-    lcd.print("Connect. server");
-    lcd.setCursor(0,1);
-    lcd.print("KO!");
-  }
-  delay(1500);
-  lcd.clear();
+	if (client.connect(IPADDRESS_SERVER, PORT))
+	{
+		lcd.clear();
+		lcd.print("Connect. server");
+		lcd.setCursor(0,1);
+		lcd.print("OK!");
+		turnOnLed(0,0,255);
+	}
+	else
+	{
+		lcd.clear();
+		lcd.print("Connect. server");
+		lcd.setCursor(0,1);
+		lcd.print("KO!");
+	}
+	delay(1500);
+	lcd.clear();
 }
 
 /*
  * Setup and connect To the wifi which is define in arduinoSecret.h
  * It will try every 5 seconds until the connection is established.
  */
-void  setupAndConnectWifi(void)
+void	setupAndConnectWifi(void)
 {
-  char ssid[] = SECRET_SSID;
-  char pass[] = SECRET_PASS;
-  String fv = WiFi.firmwareVersion();
-  
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-  Serial.print("Attempting to connect to SSID: ");
-  Serial.println(ssid);
-  while (status != WL_CONNECTED) {
-    status = WiFi.begin(ssid, pass);
-    delay(100);
-  }
-  Serial.println("Connected to WiFi");
-  lcd.print("Wifi connect.");
-  lcd.setCursor(0,1);
-  lcd.print("OK!");
-  delay(1500);
-  lcd.clear();
-  printWiFiStatus();
+	char ssid[] = SECRET_SSID;
+	char pass[] = SECRET_PASS;
+	String fv = WiFi.firmwareVersion();
+
+	if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+	{
+		lcd.clear();
+		lcd.print("Please upgrade");
+		lcd.setCursor(0,1);
+		lcd.print("the firmware");
+	}
+	while (status != WL_CONNECTED)
+	{
+		status = WiFi.begin(ssid, pass);
+		delay(100);
+	}
+	lcd.print("Wifi connect.");
+	lcd.setCursor(0,1);
+	lcd.print("OK!");
+	delay(1500);
+	lcd.clear();
 }
 
 /*
  * Setup the NFC Reader. If it's not connected after 10 tries.
  *  throw an infinite loop to force to restart the arduino.
  */
-void setupNfcReader(void)
+void	setupNfcReader(void)
 {
-  nfc.begin();
-  uint32_t versiondata = 0;
-  int tries = 0;
+	nfc.begin();
+	uint32_t versiondata = 0;
+	int tries = 0;
 
-  Serial.println("Looking for PN53x board ...");
-  while (!versiondata && tries++ < 10) {
-    versiondata = nfc.getFirmwareVersion();
-  }
+	while (!versiondata && tries++ < 10)
+	{
+		versiondata = nfc.getFirmwareVersion();
+	}
+	while (!versiondata)
+	{
+		lcd.print("NFC Reader");
+		lcd.setCursor(0,1);
+		lcd.print("KO!");
+		while(1);
+	}
 
-  while (!versiondata) {
-    Serial.println("Failed to find PN53x board.");
-    lcd.print("NFC Reader");
-    lcd.setCursor(0,1);
-    lcd.print("KO!");
-    while(1);
-  }
+	// Set the max number of retry attempts to read from a card
+	// This prevents us from waiting forever for a card, which is
+	// the default behaviour of the PN532.
+	nfc.setPassiveActivationRetries(0xFF);
 
-  // Set the max number of retry attempts to read from a card
-  // This prevents us from waiting forever for a card, which is
-  // the default behaviour of the PN532.
-  nfc.setPassiveActivationRetries(0xFF);
-
-  // configure board to read RFID tags
-  nfc.SAMConfig();
-  lcd.print("NFC reader");
-  lcd.setCursor(0,1);
-  lcd.print("OK!");
-  delay(1500);
-  lcd.clear();
-}
-
-
-/*
- * Print the informations of the current wifi connection of the Arduino.
- */
-void printWiFiStatus() {
-	// print the SSID of the network you're attached to:
-	Serial.print("SSID: ");
-	Serial.println(WiFi.SSID());
-
-	// print your board's IP address:
-	IPAddress ip = WiFi.localIP();
-	Serial.print("IP Address: ");
-	Serial.println(ip);
-
-	// print the received signal strength:
-	long rssi = WiFi.RSSI();
-	Serial.print("signal strength (RSSI):");
-	Serial.print(rssi);
-	Serial.println(" dBm");
+	// configure board to read RFID tags
+	nfc.SAMConfig();
+	lcd.print("NFC reader");
+	lcd.setCursor(0,1);
+	lcd.print("OK!");
+	delay(1500);
+	lcd.clear();
 }
 
 /*
@@ -526,35 +492,35 @@ void printWiFiStatus() {
  * @param uint8_t green : green color.
  * @param uint8_t blue : blue color.
  */
-void  turnOnLed(uint8_t red, uint8_t green, uint8_t blue)
+void	turnOnLed(uint8_t red, uint8_t green, uint8_t blue)
 {
-  WiFiDrv::analogWrite(RED, red);
-  WiFiDrv::analogWrite(GREEN, green);
-  WiFiDrv::analogWrite(BLUE, blue);
+	WiFiDrv::analogWrite(RED, red);
+	WiFiDrv::analogWrite(GREEN, green);
+	WiFiDrv::analogWrite(BLUE, blue);
 }
 
 /*
  * Play a success sound on the piezzo buzzer
  */
-void  playSuccessBuzzer(void)
+void	playSuccessBuzzer(void)
 {
-  tone(BUZZER, 987);
-  delay(100);
-  tone(BUZZER, 1318);
-  delay(300);
-  noTone(BUZZER);
+	tone(BUZZER, 987);
+	delay(100);
+	tone(BUZZER, 1318);
+	delay(300);
+	noTone(BUZZER);
 }
 
 /*
  * Play a fail sound on the piezzo buzzer.
  */
-void  playFailureBuzzer(void)
+void	playFailureBuzzer(void)
 {
-  tone(BUZZER, 100);
-  delay(200);
-  tone(BUZZER, 100);
-  delay(200);
-  noTone(BUZZER);
+	tone(BUZZER, 100);
+	delay(200);
+	tone(BUZZER, 100);
+	delay(200);
+	noTone(BUZZER);
 }
  
 
@@ -565,34 +531,32 @@ void  playFailureBuzzer(void)
  * @return : String : the UID of the card or ERROR if something 
  *  went wrong (error reading card or timeout).
  */
-String  readCardUID(String modes[], int currentMode)
+String	readCardUID(String modes[], int currentMode)
 {
-  boolean success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-  int len = 0;
-  char id[20];
-  
-  //  Waiting for a card to be scanned
-  lcd.clear();
-  lcd.print("Scan a badge...");
-  lcd.setCursor(0,1);
-  lcd.print("mode = " + modes[currentMode]);
-  delay(1000);
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
-  if (success) {
-    Serial.println("Found a card!");
-    for (uint8_t i=0; i < uidLength - 1; i++) 
-    {
-      len += sprintf(id + len, "%X:", uid[i]);
-    }
-    sprintf(id + len, "%X", uid[uidLength - 1]);
-    Serial.print("UID Value: ");
-    Serial.println(id);
-    return (String(id));
-  }
-  turnOnLed(0,0,0);
-  delay(200);
-  turnOnLed(0,0,255);
-  return ("ERROR");
+	boolean success;
+	uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+	uint8_t uidLength;        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+	int len = 0;
+	char id[20];
+
+	//Waiting for a card to be scanned
+	lcd.clear();
+	lcd.print("Scan a badge...");
+	lcd.setCursor(0,1);
+	lcd.print("mode = " + modes[currentMode]);
+	delay(1000);
+	success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+	if (success)
+	{
+		for (uint8_t i=0; i < uidLength - 1; i++)
+		{
+			len += sprintf(id + len, "%X:", uid[i]);
+		}
+		sprintf(id + len, "%X", uid[uidLength - 1]);
+		return (String(id));
+	}
+	turnOnLed(0,0,0);
+	delay(200);
+	turnOnLed(0,0,255);
+	return ("ERROR");
 }
