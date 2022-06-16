a54814c3ad8fd681bac67296661a242336cf1564
Aleksandr Buzdin <buzdin.alexander@gmail.com>
2022-06-16 14:19:58 +0200

    formatting

--- webapp/pages/forms.py
+++ webapp/pages/forms.py
@@ -3,17 +3,24 @@ from django.forms import ModelForm
 from .models import Event, Mode
 
 class ModeForm(ModelForm):
-	type = forms.CharField(required=False)
-	amount = forms.IntegerField(required=False)
+	# type = forms.CharField(required=False)
+	# amount = forms.IntegerField(required=False)
 	class Meta:
 		model = Mode
 		fields = ('type', 'amount')
+		labels = {
+			'type': 'Enter type of drink/snack',
+			'amount': 'Enter its amount',
+		}
+		widgets = {
+			'type': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Drink/snack type', 'required': False}),
+			'amount': forms.NumberInput(attrs={'class':'form-control', 'placeholder': 'Amount', 'required': False}),
+		}
 
 class EventForm(ModelForm):
 	class Meta:
 		model = Event
 		fields = ('name', 'date', 'end')
-
 		labels = {
 			'name': 'Enter the event name',
 			'date': 'Automatically generated date',
@@ -21,6 +28,6 @@ class EventForm(ModelForm):
 		}
 		widgets = {
 			'name': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Event name'}),
-			'date': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Date'}),
-			'end': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'End date'}),
+			'date': forms.DateTimeInput(attrs={'class':'form-control', 'placeholder': 'Date'}),
+			'end': forms.DateTimeInput(attrs={'class':'form-control', 'placeholder': 'End date'}),
 		}
