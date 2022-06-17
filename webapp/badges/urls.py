0f5e7bdd57d1cf111b0dd49ba0186150d99d96c5
Zeno <Zeno@bocal-5.s19.be>
2022-06-17 14:55:28 +0200

    Merge async

---
+++ webapp/badges/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from badges.views import add_student, update_student, students, testing_student
+
+app_name = 'badges'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('students/', students, name = 'students'),
+    path('add_student/', add_student, name = 'add_student'),
+    path('update_student/<int:student_id>/', update_student, name = 'update_student'),
+    path('test_student/', testing_student, name = 'test_student')
+]
---
+++ webapp/badges/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from .models import Student, Badge, StudentBadge
+from .forms import StudentForm
+from accounts.models import User
+import json
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+from .tasks import update_students
+
+def	students(request, *args, **kwargs):
+	context = {
+		'students': Student.objects.all(),
+	}
+	return render(request, "students.html", context)
+
+def	add_student(request):
+	submitted = False
+	if request.method == "POST":
+		form = StudentForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/add_student?submitted=True')
+	else:
+		form = StudentForm()
+		if 'submitted' in request.GET:
+			submitted = True
+	return render(request, "add_student.html", {'form': form, 'submitted': submitted})
+
+def	update_student(request, student_id):
+	student = Student.objects.get(pk=student_id)
+	form = StudentForm(request.POST or None, instance=student)
+	if form.is_valid():
+		form.save()
+		return redirect('badges:students')
+	return render(request, "update_student.html", {'student': student, 'form': form})
+
+def testing_student(request):
+    update_students()
+    return HttpResponse("tested")
